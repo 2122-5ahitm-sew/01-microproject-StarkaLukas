4be225abfce265a57a33b9f767b0388d9060fcb9
StarkaLukas <lstarka141000@gmail.com>
2021-09-26 13:58:13 +0200

    starka - create Brand enum

---
+++ backend/src/main/java/at/htl/kfz/entity/Brand.java
@@ -0,0 +1,25 @@
+package at.htl.kfz.entity;
+
+public enum Brand {
+    TOYOTA,
+    MERCEDES,
+    FORD,
+    SKODA,
+    CITROEN,
+    BMW,
+    SUBARU,
+    PORSCHE,
+    FERRARI,
+    VW,
+    MITSUBISHI,
+    HONDA,
+    MAZDA,
+    HYUNDAI,
+    CHEVROLET,
+    KIA,
+    OPEL,
+    SEAT,
+    FIAT,
+    LEXUS,
+    AUDI
+}
